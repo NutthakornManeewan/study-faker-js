2a6cf642b6db04c8804eac174d411b7a451ea211
NutthakornManeewan <nutthakorn.m@vsni.co.uk>
2019-11-14 16:31:23 +0700

    :recycle: Separate create-project function
    Move create-fake-project function to the 'FakeComponent' directory for convenient categorized.

---
+++ FakeComponent/fakeCreateProject.js
@@ -0,0 +1,19 @@
+const faker = require("faker");
+const randomInt = require("../functions/randomInt");
+const roleRandom = require("../functions/roleRandom");
+const projectKeyword = require("../functions/projectKeyword");
+const refUrls = require("../functions/referenceMethod");
+
+module.exports = () => {
+    const fakeCreateProject = {
+        project_name: faker.commerce.productName(),
+        role_level: roleRandom(),
+        project_description: randomInt(2) ? faker.lorem.sentences() : "",
+        project_keyword: randomInt(2) ? projectKeyword(randomInt(6)) : "",
+        reference_methodUrl: randomInt(2) ? refUrls(randomInt(6)) : "",
+        project_source: randomInt(2) ? faker.lorem.text() : "",
+        research_area: randomInt(2) ? faker.name.jobArea() : "",
+        video_url: randomInt(2) ? faker.internet.url() : "",
+    };
+    return fakeCreateProject;
+};
--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const faker = require("faker");
-const randomInt = require("./functions/randomInt");
-const projectKeyword = require("./functions/projectKeyword");
-const roleRandom = require("./functions/roleRandom");
-const refUrls = require("./functions/referenceMethod");
-const fakeUpdateProject = require("./FakeComponent/fakeUpdateProject");
 faker.seed(Date.now());
+const fakeUpdateProject = require("./FakeComponent/fakeUpdateProject");
+const fakeCreateProject = require("./FakeComponent/fakeCreateProject");
 
-let update_payload = fakeUpdateProject();
-const fakeCreateProject = {
-    project_name: faker.commerce.productName(),
-    role_level: roleRandom(),
-    project_description: randomInt(2) ? faker.lorem.sentences() : "",
-    project_keyword: randomInt(2) ? projectKeyword(randomInt(6)) : "",
-    reference_methodUrl: randomInt(2) ? refUrls(randomInt(6)) : "",
-    project_source: randomInt(2) ? faker.lorem.text() : "",
-    research_area: randomInt(2) ? faker.name.jobArea() : "",
-    video_url: randomInt(2) ? faker.internet.url() : "",
-};
+const update_payload = fakeUpdateProject();
+const create_payload = fakeCreateProject();
 
-console.log("create-project >>>", fakeCreateProject);
+console.log("create-project >>>", create_payload);
 console.log("update-project >>>", update_payload);
